3efd1d1c6dc3637c04d0ed42b286fa2886d24b56
David Hébert <pro.david.hebert@gmail.com>
2020-10-05 07:21:35 +0200

    Add burger menu

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -4,11 +4,17 @@ import Nav from 'react-bootstrap/Nav';
 import { NavLink } from 'react-router-dom';
 
 const Menu = () => (
-    <Navbar bg="dark" variant="dark">
-        <Nav>
-            <Nav.Link as={NavLink} to="/generations">Générations</Nav.Link>
-            <Nav.Link as={NavLink} to="/agents">Agents</Nav.Link>
-        </Nav>
+    <Navbar bg="dark" variant="dark" expand="sm">
+        <Navbar.Toggle />
+        <Navbar.Collapse>
+            <Nav>
+                <Nav.Link as={NavLink} to="/generations">Générations</Nav.Link>
+                <Nav.Link as={NavLink} to="/agents">Agents</Nav.Link>
+            </Nav>
+            <Nav className="ml-auto">
+                <Nav.Link>Vider la base</Nav.Link>
+            </Nav>
+        </Navbar.Collapse>
     </Navbar>
 );
 
